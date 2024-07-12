44da0c4adf230e9164ff1d5bc35fab108a82fbe9
skylexwilly <merngowilson2@gmail.com>
2024-07-12 14:04:27 +0300

    looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function countDown(startingNumber) {
+  let currentNumber = startingNumber;
+
+  while (currentNumber >= 0) {
+      console.log(currentNumber);
+      currentNumber--;
+  }
+}
+
+
+
+
+function writeCards(namesArray, eventName) {
+  let thankYouMessages = [];
+  for (let i = 0; i < namesArray.length; i++) {
+      thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return thankYouMessages
+}
